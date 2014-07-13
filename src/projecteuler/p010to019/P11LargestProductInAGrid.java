cb85635a9f944f7b1e7038774be3e6b665501dc2
Jeremy Beightol <jdbeightol@gmail.com>
2014-07-13 18:25:57 -0400

    Minor update.

--- src/projecteuler/p010to019/P11LargestProductInAGrid.java
+++ src/projecteuler/p010to019/P11LargestProductInAGrid.java
@@ -3,6 +3,8 @@ package projecteuler.p010to019;
 public class P11LargestProductInAGrid
 {
 /*
+ * Germany won the 2014 World Cup.
+ * 
  * In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
  * 
  * 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
