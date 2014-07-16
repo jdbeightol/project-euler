45eed4c81a86572bf4cc8a08de39301db86119d1
Jeremy Beightol <jdbeightol@gmail.com>
2014-07-16 05:19:05 -0400

    Reset the input number to 600,851,475,143

--- src/projecteuler/p001to009/P3LargestPrimeFactor.java
+++ src/projecteuler/p001to009/P3LargestPrimeFactor.java
@@ -11,7 +11,7 @@ public class P3LargestPrimeFactor
  * What is the largest prime factor of the number 600851475143?
  */
 	
-	private static long INPUTNUMBER = Long.MAX_VALUE;
+	private static long INPUTNUMBER = 600851475143L;
 	
 	private static ArrayList<Long> factorArray = new ArrayList<Long>();
 	
